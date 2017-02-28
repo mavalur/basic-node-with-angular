76ad93119ff4ecf69d7c353edc85065b424c05c1
Mavalur <harikumar.mavalur@gmail.com>
2017-02-28 01:17:28 -0500

    decent output. will render the ui piece later.

--- routes/processor.svc.js
+++ routes/processor.svc.js
@@ -4,40 +4,23 @@
 var lodash = require('lodash');
 var fs = require('fs');
 
-
-var indexMap = {};
+//placeholder for indexed data
+var indexMap = null;
+//placeholder for results
 var productListingCollection = [];
+// file details
+var file = {
+    products: "../data/products.txt",
+    listings: "../data/listings.txt",
+    results: "../data/results.txt"
+};
 
-
-var processor = {
-    optimizeName: function (name) {
-        if (!name && name == null) {
-            return;
-        }
-        name = name.replace(/\s+/g, '_').toUpperCase();
-        return name;
-    },
-    indexListing: function () {
-        fs.readFile('../data/listings.txt', function (err, data) {
-            if (err) throw err;
-            var array = data.toString().split("\n");
-            for (i in array) {
-                if (array[i] && array[i] != null)
-                    var listing = JSON.parse(array[i]);
-                var manufacturerNm = processor.optimizeName(listing.manufacturer);
-                manufacturerNm = (manufacturerNm == null || manufacturerNm.trim() == "") ? "OTHERS" : manufacturerNm;
-                if (!indexMap[manufacturerNm]) {
-                    indexMap[manufacturerNm] = [];
-                }
-                indexMap[manufacturerNm].push(listing);
-            }
-           // console.log(indexMap);
-            processor.digestProducts();
-        });
-
-    },
-    digestProducts: function () {
-        fs.readFile('../data/products.txt', function (err, data) {
+var search = {
+    /*
+     Searches all products and its listings
+     */
+    searchByProduct: function () {
+        fs.readFile(file.products, function (err, data) {
             if (err) throw err;
             var array = data.toString().split("\n");
             var otherCollectionListing = indexMap["OTHERS"] || [];
@@ -46,30 +29,117 @@ var processor = {
                     var product = JSON.parse(array[i]);
                     var listingByManufacturer = indexMap[processor.optimizeName(product.manufacturer)] || [];
                     var listingsForMatching = listingByManufacturer.concat(otherCollectionListing);
-                    var productKeyWords = [processor.optimizeName(product.model)];
-                    var name = processor.optimizeName(product.product_name);
-                    var regex = new RegExp('/'+product.product_name+'|'+product.model+'/ig');
-
-                    name = name.replace(regex,'');
-                    console.log(product.product_name+" => "+ name) ;
-
-                    var matches = listingsForMatching.filter(function(listing, index) {
-                        return listing && listing.title && product.model && listing.title.indexOf(product.model)!=-1;
-
-                    }, product);
 
                     productListingCollection.push({
                         name: product.product_name,
-                        listings: matches
+                        listings: search.searchByTerm(search.sanitize(product), listingsForMatching)
                     });
                 }
             }
-            fs.writeFile("../data/results.json",JSON.stringify(productListingCollection),function(err,res){
-                console.log(err);
+            if (fs.existsSync(file.results)) {
+                fs.unlinkSync(file.results);
+            }
+            ;
+            productListingCollection.forEach(function (eachProduct, idx) {
+                fs.appendFile(file.results, JSON.stringify(eachProduct).concat("\n"));
             });
+            /*fs.writeFile("../data/results.txt", JSON.stringify(productListingCollection).replace("},","\n"), function (err, res) {
+             console.log(err);
+             });*/
         });
+    },
+    /*
+     searches a given product name (or) a term
+     */
+    searchByTerm: function (searchTerm, arrayForListings) {
+        if (!arrayForListings && !searchTerm) {
+            console.log("inputs not appropriate ...", searchTerm, arrayForListings);
+            return [];
+        }
+        var searchTerms = searchTerm.split(' ');
+        var r = '^';
+        searchTerms.forEach(function (term, idx) {
+            r = r + "(?=.*" + term + ")";
+        });
+        r = r + ".*$";
+        //console.log(searchTerm + " => " + r);
+        var regEx = new RegExp(r, 'im');
+        var matches = arrayForListings.filter(function (listing, index) {
+            /*//  console.log(search.sanitize(listing.title));
+             var listResult = regEx.test(search.sanitize(listing.title));
+             if(!listResult){
+             console.log(listing.title+" = >"+regEx);
+             }
+             return listResult;*/
+            return listing && listing.title && regEx.test(search.sanitize(listing.title));
+        });
+        return matches;
+    },
+    /*
+     function to streamline the product and listing for better search.
+     replaces multiple spaces, underscore and hyphens to single space and returns. can accept a product object (or) a string
+     */
+    sanitize: function (product) {
+        var term = '';
+        if (typeof product === "object") {
+            term = product.product_name;
+        } else {
+            term = product
+        }
+        return term.replace(/[\s+_-]/ig, ' ');
     }
+};
+
+/*
+ exported function to be visible in the parent module.
+ */
+var processor = {
+    /*
+     function to streamline the indexing part to store listings per manufacturer.
+     Given a manufacture name, replaces multiple spaces with underscores and converts to uppercase. (Useful to search in the map and follow standard convention)
+     */
+    optimizeName: function (name) {
+        if (!name && name == null) {
+            return '';
+        }
+        name = name.replace(/\s+/g, '_').toUpperCase();
+        return name;
+    },
+    /*
+        Only service contract to be accessed by the UI. If no search term is given, produces the results for all products from @ref(file.products.txt)
+     */
+    search: function (optionalSearchTerm) {
+        if (indexMap == null) {
+            fs.readFile(file.listings, function (err, data) {
+                if (err) throw err;
+                var array = data.toString().split("\n");
+                indexMap = {};
+                for (i in array) {
+                    if (array[i] && array[i] != null)
+                        var listing = JSON.parse(array[i]);
+                    var manufacturerNm = processor.optimizeName(listing.manufacturer);
+                    manufacturerNm = (manufacturerNm == null || manufacturerNm.trim() == "") ? "OTHERS" : manufacturerNm;
+                    if (!indexMap[manufacturerNm]) {
+                        indexMap[manufacturerNm] = [];
+                    }
+                    indexMap[manufacturerNm].push(listing);
+                }
+                if (optionalSearchTerm && optionalSearchTerm != null) {
+                    search.searchByTerm(optionalSearchTerm, array);
+                } else {
+                    search.searchByProduct();
+                }
+            });
+        } else {
+            if (optionalSearchTerm && optionalSearchTerm != null) {
+                search.searchByTerm(optionalSearchTerm, array);
+            } else {
+                search.searchByProduct();
+            }
+        }
+    }
+
 }
 
-processor.indexListing();
+processor.search();
 //module.exports(processor);
